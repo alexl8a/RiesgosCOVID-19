d2dd55de998331613f219f0635299b6d12df67b3
Luis Iturrios <liturrios@kionetworks.com>
2020-05-07 09:39:38 -0500

    Quitar loger

--- src/riesgos/settings.py
+++ src/riesgos/settings.py
@@ -225,26 +225,26 @@ else:
     WKHTMLTOPDF_DEBUG = True
 
 #·Logging
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'WARNING',
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
-            'propagate': False,
-        },
-    },
-}
+# LOGGING = {
+#    'version': 1,
+#    'disable_existing_loggers': False,
+#    'handlers': {
+#        'console': {
+#            'class': 'logging.StreamHandler',
+#        },
+#    },
+#    'root': {
+#        'handlers': ['console'],
+#        'level': 'WARNING',
+#    },
+#    'loggers': {
+#        'django': {
+#            'handlers': ['console'],
+#            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+#            'propagate': False,
+#        },
+#    },
+#}
 
 # LOAD LOCAL SETTINGS
 try:
